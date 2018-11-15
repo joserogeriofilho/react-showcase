c8f892bf43b100ee6a319c244faeff7fb371cf11
Roger <roger.sama@gmail.com>
2018-11-15 00:39:15 -0300

    Creation of the first components: Content and Sidebar. Still figuring out the file structure for this project.

---
+++ src/components/Content.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+export class Content extends Component {
+
+    render(){
+        return (
+            <div className="content container-fluid">
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="jumbotron">
+                            <h1 className="display-4">Hello, world!</h1>
+                            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                            <hr className="my-4" />
+                            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+export class Sidebar extends Component {
+
+    render(){
+        return <div className="sidebar"></div>;
+    }
+
+}
