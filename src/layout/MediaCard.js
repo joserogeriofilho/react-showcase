31089b56727ff644ed5d1ed4130bbc7a4ce13fe7
Jose Rogerio Bezerra Barbosa Filho - DATAPREVRN <joserogerio.filho@dataprev.gov.br>
2019-04-03 14:28:14 -0300

    Styling of cards in the material theme

--- src/layout/MediaCard.js
+++ src/layout/MediaCard.js
@@ -3,10 +3,18 @@ import React from 'react';
 export function MediaCard(props) {
     return (
         <div key={props.id} className="col-sm">
-            <div className="media-card">
-                <span className="overline">{props.image}</span>
-                <h5>{props.title}</h5>
-                <p>{props.description}</p>
+            <div className="card card-100-height">
+                <div className="card-rich-media">
+                    <img src={props.image} />
+                </div>
+
+                <div className="card-primary-title">
+                    <h6>{props.title}</h6>
+                </div>
+
+                <div className="card-supporting-text">
+                    <p>{props.description}</p>
+                </div>
             </div>
         </div>
     );
