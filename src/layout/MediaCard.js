7116c269825ee441905e580f72b292046e25294c
Roger <roger.sama@gmail.com>
2019-04-03 20:40:29 -0300

    Continuation of the card styling plus some button styles as well.

--- src/layout/MediaCard.js
+++ src/layout/MediaCard.js
@@ -10,10 +10,16 @@ export function MediaCard(props) {
 
                 <div className="card-primary-title">
                     <h6>{props.title}</h6>
+                    <span className="secondary-text">Lorem ipsum et dolorem</span>
                 </div>
 
                 <div className="card-supporting-text">
                     <p>{props.description}</p>
+                </div>
+
+                <div className="card-actions">
+                    <a className="btn btn-flat btn-primary">ACTION 1</a>
+                    <a className="btn btn-flat btn-primary">ACTION 2</a>
                 </div>
             </div>
         </div>
