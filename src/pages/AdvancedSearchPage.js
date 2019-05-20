6a3b9e8aca20a1ac29a1e18866ff4932ba7058bc
Jose Rogerio Bezerra Barbosa Filho - DATAPREVRN <joserogerio.filho@NTLD279425.GOV>
2019-05-20 17:10:43 -0300

    Updating the home page

--- src/pages/AdvancedSearchPage.js
+++ src/pages/AdvancedSearchPage.js
@@ -5,14 +5,15 @@ export function AdvancedSearchPage(props) {
         <div className="content container-fluid">
             <div className="row justify-content-md-center mb-5 hero-section">
                 <div className="col-xs-12 col-xl-8 text-center">
-                    <h5 className="mb-2">A Simple Yet Complete ReactJS Application</h5>
+                    <h4>React Showcase</h4>
+                    <h6 className="mb-4">A simple yet complete ReactJS application</h6>
                     <p>This application provides a user registration screen with insertion and deletion of entries plus a screen with a grid of media cards. All the data is obtained by HTTP requests to a mocked REST API using the json-server library. The routing was made with the react-route library.</p>
                     <img src="./img/devices-mockup.png" className="img-fluid" alt="Mockup image of the showcase application in multiple devices."></img>
                 </div>
             </div>
 
             <div className="row justify-content-md-center mb-4">
-                <div className="col-xs-12 col-xl-6 mb-4">
+                <div className="col-xs-12 col-xl-8 mb-4">
                     <div className="card card-100-height">
                         <div className="card-body">
                             <h5 className="mb-2">Components, Functionalities and Patterns</h5>
@@ -27,7 +28,7 @@ export function AdvancedSearchPage(props) {
                     </div>
                 </div>
 
-                <div className="col-xs-12 col-xl-6 mb-4">
+                <div className="col-xs-12 col-xl-8 mb-4">
                     <div className="card card-100-height">
                         <div className="card-body">
                             <h5 className="mb-2">Installing and Running</h5>
