2a9b5a6d98134d30e2d8bc377c55c80094175c21
José Rogério Filho <roger.sama@gmail.com>
2022-11-20 22:09:57 +0100

    Updates

---
+++ precache-manifest.575a14e255215d959e5164d9ba4b3dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb527c5064609da812a",
+    "url": "/react-showcase/static/css/main.07cc9da2.chunk.css"
+  },
+  {
+    "revision": "acb527c5064609da812a",
+    "url": "/react-showcase/static/js/main.acb527c5.chunk.js"
+  },
+  {
+    "revision": "a207768220eb89f25750",
+    "url": "/react-showcase/static/js/1.a2077682.chunk.js"
+  },
+  {
+    "revision": "cc2a1d9f0a86c7cd06b1",
+    "url": "/react-showcase/static/js/runtime~main.cc2a1d9f.js"
+  },
+  {
+    "revision": "e09f83a2524e831b7b56814515df963d",
+    "url": "/react-showcase/index.html"
+  }
+];
